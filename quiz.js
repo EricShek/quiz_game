634ac2dedb74721b2f626ba95e10310d4f715cd0
Eric Shek <ericshek23@gmail.com>
2022-06-01 10:55:23 -0400

    added pseudo

---
+++ quiz.js
@@ -0,0 +1,11 @@
+/*
+Step 1: display start page including title, start button and paragraph
+
+Step 2 : timer will start when the start button clicked from start page, display question with 4 mulitple choices questions including buttons. Once start button is press main page hide only show questions. Eash question will have 15 seconds.  when clicked answer will show correct or wrong.  if wrong answer selected, 15 seconds will be taken away from timer.
+
+Step 3: Once all questions answer, input needed from user for initial with submit button. timer stop and time left will be the score of the quiz.
+
+Step 4: show dashboard of all high score
+
+
+ */
